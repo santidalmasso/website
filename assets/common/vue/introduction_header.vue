82a0cc078925920996aa1b7f38511eadee2498d0
Eric Crooks <eric.crooks@gmail.com>
2020-11-09 09:20:22 -0500

    [#versioned-docs] make sure version is included in versioned docs

--- assets/common/vue/introduction_header.vue
+++ assets/common/vue/introduction_header.vue
@@ -10,6 +10,15 @@ export default {
       required: true
     },
   },
+  methods: {
+    versioned() {
+      const version = window.location.href.match(/v.+[0-9]/g);
+      if (version) {
+        return " " + version[0];
+      }
+      return false;
+    }
+  }
 }
 </script>
 
@@ -17,5 +26,6 @@ export default {
 div
   h1.text-5xl
     strong {{ heading }}
+      span(v-show="versioned()") {{ versioned() }}
   p {{ description }}
 </template>
