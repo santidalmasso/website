1fcaed2120f915dff9284e9e8f121e3c65f7c0e7
Eric Crooks <eric.crooks@gmail.com>
2020-11-08 22:44:53 -0500

    [#versioned-docs] add drash v1.3.0 docs to drash page

--- drash/vue/app.vue
+++ drash/vue/app.vue
@@ -87,6 +87,9 @@ export default {
             "Content Negotiation": "/advanced-tutorials/content-negotiation",
             "Using Drash With Docker Compose": "/advanced-tutorials/using-drash-with-docker-compose",
           },
+          "Versions": {
+            "v1.3.0": "/drash/v1.3.0",
+          }
         },
         module: "drash",
       }
