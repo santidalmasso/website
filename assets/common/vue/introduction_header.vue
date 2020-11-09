39699db9e240b313e22b215610a51e1d405c42bf
Eric Crooks <eric.crooks@gmail.com>
2020-11-09 16:39:39 -0500

    [#versioned-docs] add comment

--- assets/common/vue/introduction_header.vue
+++ assets/common/vue/introduction_header.vue
@@ -11,6 +11,11 @@ export default {
     },
   },
   methods: {
+    /**
+     * Show the title with the version if the URL has /vx.x.x where x.x.x is a
+     * version number. For example, if someone goes to /drash/v1.3.0, then the
+     * title should show up as Drash v1.3.0.
+     */
     versioned() {
       const version = window.location.href.match(/v.+[0-9]/g);
       if (version) {
